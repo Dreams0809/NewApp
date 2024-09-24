5942cd6c1b8d3a08292063fd1709b2c731aab4c5
Dreams0809 <expensivepain9@gmail.com>
2024-09-24 18:07:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './css/App.css'
+import {BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom'
+import Homescreen from './pages/Homescreen'
 
-function App() {
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <BrowserRouter>
+        <Routes>
+          { <Route index element={Homescreen} /> }
+          { <Route path='/Homescreen' element={<Homescreen />} /> }
+        </Routes>
+      </BrowserRouter>
+
+
+
+      <h1> Hi Welcome to Flash</h1>
+      <h1> Create Community and Run it!</h1>
+
+
+      <a href='/Homescreen' className='container'> 
+      <BrowserRouter>
+        <Routes>
+          {/* { <Route index element={Homescreen} /> } */}
+          { <Route path='/Homescreen' element={<Homescreen />} /> }
+        </Routes>
+      </BrowserRouter>
+
+      Sign</a>
+      
+      <a href='/' className='container'>Login </a>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/pages/Homescreen.js
@@ -0,0 +1,10 @@
+import Header from '../Components/Header'
+
+export default function(){
+    return(
+        <div> 
+            <Header />
+            <h1> Home screen </h1>
+        </div>
+    )
+}
