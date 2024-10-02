4ec95cfedffafbca7d906a9a7d7897a9f454d942
Dreams0809 <expensivepain9@gmail.com>
2024-10-01 22:03:08 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Home from './pages/Home'
 // import Create from './pages/Create'
 // import Profile from './pages/Profile'
 import { Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import Signin from './Components/Signin'
+
 
 
 
@@ -17,13 +19,17 @@ export default function App() {
       <h1> Create Community and Run it!</h1>
       
       
-         <Routes>
-              {/* Home component is displayed when the /home path is visited *  */}
-              <Route path="/home" element={<Home />}/>
+        <Routes>
+            {/* Home component is displayed when the /home path is visited *  */}
+            <Route path="/home" element={<Home />}/>
 
-              {/* Add more routes here */}
-              <Route path='/login' element={<Signin />}/>
-          </Routes>
+            {/* Add more routes here */}
+            <Route path='/Signin' element={<Signin />}/>
+        </Routes>
+
+          <Link to="/Signin" style={{textDecoration: 'none'}}>
+            <button>To login page</button>
+          </Link>
       
 
     </div>
--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -1,10 +1,56 @@
 import React from "react";
+import '../css/Signin.css';
+
 
 
 export default function Login(){
     return(
-        <div>
-            <h1> This is Login page.</h1>
-        </div>
+       
+    <div className="login-box">
+        <h2>Sign-Up</h2>
+
+        
+       <form action="/signup" method="POST"></form>
+
+        <h3 className="label">User Name:</h3>
+        <input type="text" name="userName" placeholder="User Name" required />
+
+        <h3>Email:</h3>
+        <input type="email" name="email" placeholder="Email" required />
+
+        <h3>Password:</h3>
+        <input
+        //   type={passwordVisible ? 'text' : 'password'}
+          name="password"
+          placeholder="Password"
+          required
+        />
+
+        <h3> Name</h3>
+        <input
+        //   type={passwordVisible ? 'text' : 'password'}
+          name="Name"
+          placeholder="Name"
+          required
+        />
+
+        <h3> Area Code</h3>
+        <input
+        //   type={passwordVisible ? 'text' : 'password'}
+          name="Area Code"
+          placeholder="Area Code"
+          required
+        />
+        {/* <i className="fas fa-eye" onClick={() => togglePasswordVisibility(setPasswordVisible)}></i> */}
+
+      
+        {/* <i className="fas fa-eye" onClick={() => togglePasswordVisibility(setConfirmPasswordVisible)}></i> */}
+
+        <input
+          type="submit"
+          id="submit"
+          style={{ height: '50px', width: '150px', paddingLeft: '10px' }}
+        />
+    </div>
     )
 }
